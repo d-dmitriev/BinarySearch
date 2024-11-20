a654d8c3099d6bf5575f0d303a8179a4982cba8e
Dmitry Dmitriev <d.dmitrieff@gmail.com>
2024-11-20 11:45:18 +0300

    init

---
+++ src/home/work/Collections.java
@@ -0,0 +1,14 @@
+package home.work;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class Collections {
+    public static <T> int binarySearch(List<? extends Comparable<? super T>> list, T key) {
+        return java.util.Collections.binarySearch(list, key);
+    }
+
+    public static <T> int binarySearch(List<? extends T> list, T key, Comparator<? super T> c) {
+        return java.util.Collections.binarySearch(list, key, c);
+    }
+}
